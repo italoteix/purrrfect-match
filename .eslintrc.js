87ab8d2f3ca5bafa8ac92b36e3646fd9c0d0aa8b
Italo <italo_bruno91@hotmail.com>
2020-03-07 23:52:48 -0300

    :wrench: refactor: fix lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,23 +3,23 @@ module.exports = {
     browser: true,
     es6: true
   },
-  extends: ["plugin:react/recommended", "airbnb", "prettier", "prettier/react"],
+  extends: ['airbnb', 'prettier', 'prettier/react'],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
-  parser: "babel-eslint",
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       jsx: true
     },
     ecmaVersion: 2018,
-    sourceType: "module"
+    sourceType: 'module'
   },
-  plugins: ["react", "prettier"],
+  plugins: ['react', 'prettier'],
   rules: {
-    "prettier/prettier": "error",
-    "react/jsx-filename-extension": [("warn": { extensions: [".jsx", ".js"] })],
-    "import/prefer-default-export": "off"
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [{ warn: { extensions: ['.jsx', '.js'] } }],
+    'import/prefer-default-export': 'off'
   }
 };
