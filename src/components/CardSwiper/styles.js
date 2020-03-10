82304f65a11c89958f7ca66d1125a40ea692a2fb
Italo <italo_bruno91@hotmail.com>
2020-03-10 19:39:39 -0300

    :lipstick: refactor: remove unused styles

--- src/components/CardSwiper/styles.js
+++ src/components/CardSwiper/styles.js
@@ -3,24 +3,25 @@ import styled from 'styled-components';
 export const Container = styled.article`
   display: flex;
   overflow-x: auto;
-  margin: -1rem;
+  margin: 0 -1rem;
 
   a {
-    display: flex;
-    flex-basis: auto;
+    border-radius: 5px;
     margin-left: 1rem;
     min-width: 60%;
-    height: auto;
-    border-radius: 5px;
     overflow: hidden;
+    padding-top: 60%;
+    position: relative;
 
-    &:before {
-      content: '';
-      padding-top: 100%;
+    &:last-child {
+      margin-right: 1rem;
     }
   }
 
   img {
+    position: absolute;
+    top: 0;
+    left: 0;
     height: 100%;
     width: 100%;
     object-fit: cover;
@@ -29,6 +30,7 @@ export const Container = styled.article`
   @media (min-width: 640px) {
     a {
       min-width: 40%;
+      padding-top: 40%;
     }
   }
 `;
