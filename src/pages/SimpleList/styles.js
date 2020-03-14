5b2ce235c618172b18fadc762388825a34322726
Italo <italo_bruno91@hotmail.com>
2020-03-14 15:00:19 -0300

    :sparkles: feat: add search functionality

--- src/pages/SimpleList/styles.js
+++ src/pages/SimpleList/styles.js
@@ -58,29 +58,3 @@ export const Header = styled.header`
     }
   }
 `;
-
-export const Search = styled.form`
-  display: flex;
-  flex: 1;
-  align-items: center;
-
-  background-color: #f3f3ff;
-  border-radius: ${props => props.theme.radius};
-  padding: 0.4rem 0.8rem;
-  margin-right: 2.5rem;
-
-  input {
-    border: none;
-    background-color: #f3f3ff;
-    font-weight: 300;
-    font-size: 15px;
-    margin-left: 0.4rem;
-
-    flex: 1;
-  }
-
-  @media (min-width: 768px) {
-    max-width: 316px;
-    margin-left: auto;
-  }
-`;
